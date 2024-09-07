905a2d1a3a70ec5492bec4d34dddc95d24576c4f
lxccc812 <lxccc812@163.com>
2024-09-07 11:23:20 +0800

    feat: day02 for循环的使用

--- src/day02/hello/hello.go
+++ src/day02/hello/hello.go
@@ -11,7 +11,11 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    message, err := greetings.Hello("Lee")
+    // message, err := greetings.Hello("Lee")
+
+    names := []string{"Gladys", "Samantha", "Darrin"}
+    message, err := greetings.Hellos(names)
+
     if err != nil {
         log.Fatal(err)
     }
